c2d5cb7d01d2f5bf6063c403763d56619027ee0e
Patrick Walter <patrick.2017316623@gmail.com>
2020-08-12 19:00:54 -0300

    Fixed Migration CreateClass

--- src/migrations/1597033307044-createClass.ts
+++ src/migrations/1597033307044-createClass.ts
@@ -1,6 +1,6 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class createClass1597033307044 implements MigrationInterface {
+export class CreateClass1597033307044 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
@@ -28,7 +28,7 @@ export class createClass1597033307044 implements MigrationInterface {
                     default: "now()"
                 },
                 {
-                    name: "updateAt",
+                    name: "updatedAt",
                     type: "timestamp",
                     default: "now()"
                 }
