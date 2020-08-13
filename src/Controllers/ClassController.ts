bce056c223c78fe0f8f4cbb516a44a6d35302076
Patrick Walter <patrick.2017316623@gmail.com>
2020-08-13 00:54:39 -0300

    ClassController - index() and show()

--- src/Controllers/ClassController.ts
+++ src/Controllers/ClassController.ts
@@ -15,6 +15,31 @@ class ClassController{
             res.status(400).json({message: "Error"});
         }
     }
+
+    async index(req: Request, res: Response){
+        try{
+            const classRepository = getRepository(Class);
+            const reposFind = await classRepository.find();
+            res.status(201).json(reposFind);
+        }
+        catch(err){
+            console.log("Error: " + err.message);
+            res.status(400).json({message: "Error"});
+        }
+    }
+
+    async show(req: Request, res: Response){
+        try{
+            const { name } = req.params; 
+            const classRepository = getRepository(Class);
+            const reposFind = await classRepository.find({ where: { name } });
+            res.status(201).json(reposFind);
+        }
+        catch(err){
+            console.log("Error: " + err.message);
+            res.status(400).json({message: "Error"});
+        }
+    }
 }
 
 export default new ClassController();
--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,7 @@ import ClassController from "./Controllers/ClassController";
 const routes = Router();
 
 routes.post("/", ClassController.create);
+routes.get("/", ClassController.index);
+routes.get("/:name", ClassController.show);
 
 export default routes;
