2b9dd1151bcc0c8953d5aad6d52d11bad948a79b
Patrick Walter <patrick.2017316623@gmail.com>
2020-08-13 23:40:58 -0300

    New Routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,22 @@
 import {Router} from "express";
 import ClassController from "./Controllers/ClassController";
+import ContentController from "./Controllers/ContentController";
+import LessonController from "./Controllers/LessonController";
+import StudentController from "./Controllers/StudentController";
 
 const routes = Router();
 
-routes.post("/", ClassController.create);
-routes.get("/", ClassController.index);
-routes.get("/:name", ClassController.show);
+routes.post("/class", ClassController.create);
+routes.get("/class", ClassController.index);
+routes.get("/class/:name", ClassController.show);
+
+routes.post("/content", ContentController.create);
+routes.get("/content", ContentController.index);
+
+routes.post("/lesson", LessonController.create);
+routes.get("/lesson", LessonController.index);
+
+routes.post("/student", StudentController.create);
+routes.get("/student", StudentController.index);
 
 export default routes;
