fc84ba24e1a7ec335842db3c2440eadc680ca0dc
Patrick Walter <patrick.2017316623@gmail.com>
2020-08-13 23:38:28 -0300

    StudentController

---
+++ src/Controllers/StudentController.ts
@@ -0,0 +1,31 @@
+import { getRepository } from "typeorm";
+import {Request, Response} from "express";
+import Student from "../models/Student";
+
+class StudentController{
+    async create(req: Request, res: Response){
+        try{
+            const repository = getRepository(Student);
+            const repoSaved = await repository.save(req.body);
+            res.status(201).json(repoSaved);
+        }
+        catch(err){
+            console.log("Error: " + err.message);
+            res.status(400).json({message: "Error"});
+        }
+    }
+
+    async index(req: Request, res: Response){
+        try{
+            const repository = getRepository(Student);
+            const reposFind = await repository.find();
+            res.status(201).json(reposFind);
+        }
+        catch(err){
+            console.log("Error: " + err.message);
+            res.status(400).json({message: "Error"});
+        }
+    }
+}
+
+export default new StudentController();
